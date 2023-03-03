27299d0c8f7eaf421aa5ffaee321de1c1beb6413
xbol0 <xbolo@duck.com>
2023-03-03 18:57:03 +0800

    export hex encoder/decoder and secp256k1

--- mod.ts
+++ mod.ts
@@ -3,3 +3,4 @@ export * from "./types.ts";
 export * as nostr from "./nostr.ts";
 export * from "./pg_repo.ts";
 export const upgradeFn = Deno.upgradeWebSocket;
+export { hex, secp256k1 } from "./deps.ts";
